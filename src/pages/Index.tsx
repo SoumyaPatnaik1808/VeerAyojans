1febdceeb0a65cc711977caef2de50f6fdc8bac7
Soumya Patnaik <patnaiksoumya20@gmail.com>
2025-12-30 23:22:10 +0530

    Mouse clicking animation added in Landing page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,14 +4,24 @@ import FeaturesSection from "@/components/FeaturesSection";
 import LogoSection from "@/components/LogoSection";
 import WhySection from "@/components/WhySection";
 import Footer from "@/components/Footer";
-
+import ClickSpark from '@/components/Animations/ClickSpark';
 
 const Index = () => {
   return (
      <> 
   
       <div className="min-h-screen bg-background">
-     
+      
+
+ <ClickSpark
+  sparkColor='#786401ff'
+  sparkSize={10}
+  sparkRadius={15}
+  sparkCount={8}
+  duration={400}
+>
+  {/* Your content here */}
+
      <div className="relative z-10">
       <Navbar />
       <main>
@@ -23,6 +33,7 @@ const Index = () => {
       </main>
       <Footer />
     </div>
+    </ClickSpark>
      </div>
    
      </>
